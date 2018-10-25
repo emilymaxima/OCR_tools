35a0255a77a1f8a658010140466dd5bf2f00d7c3
Emily <metaranha@gmail.com>
2018-10-24 22:59:31 -0400

    finalized all files. Commiting and pushing

---
+++ OCR.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+for f in *.pdf;
+do
+  echo "Running OCR on "$f""
+  ocrmypdf -v --deskew --clean --clean-final "$f" "$f";
+  
+  echo "moving ocr'd file"
+#  Make sure this value is changed to fit your destination!
+  mv "$f" /root/OCRd
+
+#  echo "removing original file"
+#  rm $f
+
+  echo "finished"
+done
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OCR_tools
-A set of tools supporting bulk OCR jobs
+A set of tools supporting bulk OCR jobs supported by Unix.
+
+In order to install system requirements, use the following command:
+xargs -rxa requirements.txt -- sudo apt-get install --
